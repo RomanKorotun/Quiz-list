558891125e251bd9725231f9bac9a6cd28f554ed
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2025-03-18 15:42:32 +0200

    add base_url backend

--- src/api/quiz.js
+++ src/api/quiz.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:6060/api";
+axios.defaults.baseURL = "https://quiz-list-backend.onrender.com";
 
 export const addQuiz = async (quiz) => {
   return await axios.post("/quizzes", quiz);
