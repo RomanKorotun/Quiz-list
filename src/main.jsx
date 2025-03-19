84b4e92d3eccf8b485431d19c9265daac22f3a23
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2025-03-19 19:01:59 +0200

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { GlobalStyle } from "./GlobalStyle.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Quiz-list">
+    <HashRouter basename="/Quiz-list">
       <GlobalStyle />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
